6b03a778dac80b646505807b324e08a1d90eb01c
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-09-20 19:26:08 +0300

    Update balance.sh

--- quil/balance.sh
+++ quil/balance.sh
@@ -2,9 +2,9 @@
 
 s=$1
 m=`echo 60/$s|bc`
-a=$(cd /root/ceremonyclient/node && ./node-1.4.21-linux-amd64 -balance | grep 'Unclaimed balance:' | egrep -o "[0-9].*$" | sed -r 's/ .+//')
+a=$(cd /root/ceremonyclient/node && ./node-1.4.21.1-linux-amd64 -balance | grep 'Unclaimed balance:' | egrep -o "[0-9].*$" | sed -r 's/ .+//')
 sleep $s
-b=$(cd /root/ceremonyclient/node && ./node-1.4.21-linux-amd64 -balance | grep 'Unclaimed balance:' | egrep -o "[0-9].*$" | sed -r 's/ .+//')
+b=$(cd /root/ceremonyclient/node && ./node-1.4.21.1-linux-amd64 -balance | grep 'Unclaimed balance:' | egrep -o "[0-9].*$" | sed -r 's/ .+//')
 c=`echo $b-$a | bc` #сложить цифры float
 
 echo Был баланс  $a , через $1 секунд стал  $b , прибыль на отрезке -  $c
