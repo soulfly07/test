08ff24fdedbcc5922ab461ceeea1766ff8b2fcc4
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-09-24 21:14:32 +0300

    Update 2_satori_install.sh

--- satori/2_satori_install.sh
+++ satori/2_satori_install.sh
@@ -2,7 +2,8 @@
 
 cd ~
 apt install unzip -y
-wget -P ~/ https://satorinet.io/static/download/linux/satori.zip && unzip ~/satori.zip && rm ~/satori.zip && cd ~/.satori
+#wget -P ~/ https://satorinet.io/static/download/linux/satori.zip && unzip ~/satori.zip && rm ~/satori.zip && cd ~/.satori
+wget -P ~/ https://github.com/soulfly07/test/raw/refs/heads/main/satori/satori.zip  && unzip ~/satori.zip && rm ~/satori.zip && cd ~/.satori
 sudo apt install -y python3.10-venv
 bash install.sh
 bash install_service.sh
