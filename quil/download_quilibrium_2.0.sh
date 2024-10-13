6d500857b556d7cf1c2179cb652144f0695669a9
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-10-13 20:39:40 +0300

    Update download_quilibrium_2.0.sh

--- quil/download_quilibrium_2.0.sh
+++ quil/download_quilibrium_2.0.sh
@@ -58,7 +58,8 @@ echo "запуск ceremonyclient ..."
 systemctl start ceremonyclient
 
 echo "Все файлы успешно скачаны, chmod +x применен к файлу $(basename "${BASE_URL}")."
-
+echo
+echo
 echo "ПРОВЕРКА ВЕРСИИ"
 echo
 journalctl -u ceremonyclient -r --no-hostname  -n 1 -g "Quilibrium Node" -o cat
