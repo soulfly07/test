f5ad1f3dfd58070d3bebe23e9a7e522d2ae19669
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-10-17 13:04:41 +0300

    Update wala-install.sh

--- wala-install.sh
+++ wala-install.sh
@@ -13,6 +13,9 @@ chmod +x *
 screen -dmS waglaylad ./waglaylad --utxoindex --rpclisten-borsh=public 
 echo "Пауза 10 секунд что бы нода загрузилась, после старта майнер может быть с 0 хешей, значит что нода еще не догнала блоки, просто ждать"
 sleep 10
-с=$2
-for i in {1..$c}; do screen -dmS waglaylaminer$i ./waglaylaminer --miningaddr $1 ; done
+
+screen -dmS waglaylaminer ./waglaylaminer --miningaddr $1
+
+#с=$2
+#for i in {1..$c}; do screen -dmS waglaylaminer$i ./waglaylaminer --miningaddr $1 ; done
 
