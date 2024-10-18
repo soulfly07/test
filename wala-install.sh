9416826872c72c0ef9f8173fd826d2be9418c079
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-10-18 08:54:45 +0300

    Create wala-install.sh

---
+++ wala-install.sh
@@ -0,0 +1,19 @@
+#echo "deb http://cz.archive.ubuntu.com/ubuntu mantic main" >> /etc/apt/sources.list && apt update && apt install libc6 -y
+
+cd /root
+wget -O Linux.-.waglayalad-rusty.zip https://github.com/user-attachments/files/17392272/Linux.-.waglayalad-rusty.zip
+unzip Linux.-.waglayalad-rusty.zip
+mkdir waglayala && mv 'Linux - waglayalad-rusty'/* waglayala
+rm -r 'Linux - waglayalad-rusty' Linux.-.waglayalad-rusty.zip
+cd waglayala 
+#wget -O waglaylaminer https://github.com/soulfly07/test/raw/refs/heads/main/waglaylaminer
+wget -O wala-miner-cpu-linux.zip https://github.com/RacerCommunity/wala-miner/releases/download/v0.1.0/wala-miner-cpu-linux.zip
+unzip -oj wala-miner-cpu-linux.zip
+wget -O waglaylawallet https://github.com/soulfly07/test/raw/refs/heads/main/waglaylawallet
+chmod +x *
+
+screen -dmS waglaylad ./waglaylad --utxoindex --rpclisten-borsh=public 
+echo "Пауза 10 секунд что бы нода загрузилась, после старта майнер может быть с 0 хешей, значит что нода еще не догнала блоки, просто ждать"
+sleep 10
+
+screen -dmS wala-miner wala-miner --mining-address $1 --wala-address 127.0.0.1 --port 12110
