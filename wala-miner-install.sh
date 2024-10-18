414026d5414b78f32ffa710c3292ba271b438153
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-10-18 14:44:49 +0300

    Update wala-miner-install.sh

--- wala-miner-install.sh
+++ wala-miner-install.sh
@@ -2,11 +2,13 @@
 #echo "deb http://cz.archive.ubuntu.com/ubuntu mantic main" >> /etc/apt/sources.list && apt update && apt install libc6
 
 cd /root
-mkdir waglayala
+mkdir -p waglayala
 cd waglayala
 wget -O wala-miner-cpu-linux.zip https://github.com/RacerCommunity/wala-miner/releases/download/v0.1.0/wala-miner-cpu-linux.zip
 unzip -oj wala-miner-cpu-linux.zip
 chmod +x *
 
-screen -dmS wala-miner1 bash -c "while true; do ./wala-miner --mining-address $1 --wala-address $2 --port 12110; done"
+# Запуск майнера в screen, передача аргументов через переменные
+screen -dmS wala-miner1 bash -c "mining_address=$1; wala_address=$2; while true; do ./wala-miner --mining-address \$mining_address --wala-address \$wala_address --port 12110; done"
+
 
