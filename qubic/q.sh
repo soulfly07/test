1093d6bade987cf16d4885219ccc12af43d6aad4
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-10-29 03:14:59 +0200

    Update q.sh

--- qubic/q.sh
+++ qubic/q.sh
@@ -3,15 +3,14 @@
 TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjVlYmI3YWQ1LTkzZjgtNDNjMi1hNDQwLThhYjE3ZTcxZTJjZSIsIk1pbmluZyI6IiIsIm5iZiI6MTcyOTgwNjA4MiwiZXhwIjoxNzYxMzQyMDgyLCJpYXQiOjE3Mjk4MDYwODIsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.JFJ1TAyrQK-AWxosipw7xPXJxAEKZ2fiM2St6ZJHmHfBCsX-iIedxNncnxT2-NN2R1sLcUpHb5SlyRxuDkKgPqm7GT2aAN2cRgn_-kLDaP8KYoUN82l5tBAbwvnLMMAlyuoLNHX7peomlUoVkdl6acbKjTveV3WbasfrXQZPe3jP8nOHMlXVPskB5_l9Xm9qYpJLIBLJ1nMVXWtN7748xPSdsYUYt31-qjHrL5yekFIDxbm_k86Egs7NjJSypLDbo4HRM-ZqAublaXofZ5lvI3m4xhQj3dS1-GUORa9M4m_q2Fuj92ikniPq0hmITk-huJXZIq4MF9QeRvW7EVAaGw" 
 
 WORKER=$(hostname) 
-#sudo su
-#apt update -y
-apt install screen -y
-apt install -y nano
+apt install sudo -y
+sudo apt install screen -y
+sudo apt install nano -y
 cd ~
-mkdir qub 
-cd qub
-wget -O quai-gpu-miner https://github.com/dominant-strategies/quai-gpu-miner/releases/download/v0.2.0/quai-gpu-miner && chmod +x quai-gpu-miner
-cd ~/qub
+mkdir qub-gpu 
+cd qub-gpu
+wget -O quai-gpu-miner https://github.com/dominant-strategies/quai-gpu-miner/releases/download/v0.3.0/quai-gpu-miner && chmod +x quai-gpu-miner
+cd ~/qub-gpu
 
 
 
@@ -26,13 +25,13 @@ cat <<EOF > appsettings.json
     "baseUrl": "https://wps.qubic.li",
     "alias": "$WORKER",
     "trainer": {
-      "cpu": true,
+      "cpu": false,
       "gpu": true,
       "gpuVersion": "CUDA12",
       "cpuVersion": "",
-      "cpuThreads": $1
+      "cpuThreads": 0
     },
-    "isPps": false,
+    "isPps": true,
     "useLiveConnection": true,
     "accessToken": "$TOKEN",
     "idleSettings": {
@@ -43,4 +42,4 @@ cat <<EOF > appsettings.json
 }
 EOF
 
-screen -S qub ./qli-Client
+screen -S qub-gpu ./qli-Client
