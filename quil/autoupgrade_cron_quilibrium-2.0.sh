340b417a50145e37a0fd0f511a8c673c8b7bd403
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-11-11 20:01:46 +0200

    Create autoupgrade_cron_quilibrium-2.0.sh

---
+++ quil/autoupgrade_cron_quilibrium-2.0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Найти папку ceremonyclient
+CEREMONY_DIR=$(find / -type d -name "ceremonyclient" 2>/dev/null | head -n 1)
+
+# Скачать скрипт папку node внутри ceremonyclient
+wget -O "$CEREMONY_DIR/node/upgrade-quilibrium-2.0.sh" https://github.com/soulfly07/test/raw/refs/heads/main/quil/upgrade-quilibrium-2.0.sh
+
+# Сделать скрипт исполняемым
+chmod +x "$CEREMONY_DIR/node/upgrade-quilibrium-2.0.sh"
+
+# Добавить задание в crontab для выполнения скрипта каждые 5 минут с логированием
+(crontab -l 2>/dev/null; echo "*/5 * * * * $CEREMONY_DIR/node/upgrade-quilibrium-2.0.sh >> $CEREMONY_DIR/node/upgrade.log 2>&1") | crontab -
