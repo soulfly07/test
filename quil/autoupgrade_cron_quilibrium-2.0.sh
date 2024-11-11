a4ae1d7575cc7522962e4b612df4a437e3c8add3
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2024-11-11 21:13:24 +0200

    Update autoupgrade_cron_quilibrium-2.0.sh

--- quil/autoupgrade_cron_quilibrium-2.0.sh
+++ quil/autoupgrade_cron_quilibrium-2.0.sh
@@ -9,6 +9,5 @@ wget -O "$CEREMONY_DIR/node/upgrade-quilibrium-2.0.sh" https://github.com/soulfl
 # Сделать скрипт исполняемым
 chmod +x "$CEREMONY_DIR/node/upgrade-quilibrium-2.0.sh"
 
-# Добавить задание в crontab для выполнения скрипта каждые 5 минут с логированием
 # Добавить задание в crontab для выполнения скрипта каждые 5 минут с логированием и меткой времени
 (crontab -l 2>/dev/null; $CEREMONY_DIR/node/upgrade-quilibrium-2.0.sh >> $CEREMONY_DIR/node/upgrade.log 2>&1)") | crontab -
